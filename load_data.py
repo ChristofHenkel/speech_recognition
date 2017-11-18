f20b0a64fad22d55cb5e556c9e1272705c948216
Christof Henkel <henkel.christof@gmail.com>
2017-11-18 18:48:20 +0100

    started working on Data Feeder Class

--- load_data.py
+++ load_data.py
@@ -1,6 +1,9 @@
 import re
 from glob import glob
 import os
+import numpy as np
+from scipy.io import wavfile
+
 
 DATADIR = 'assets/'
 POSSIBLE_LABELS = 'yes no up down left right on off stop go silence unknown'.split()
@@ -47,4 +50,78 @@ def load_data(data_dir):
     print('There are {} train and {} val samples'.format(len(train), len(val)))
     return train, val
 
+def data_generator(data, params, mode='train'):
+    def generator():
+        if mode == 'train':
+            np.random.shuffle(data)
+        # Feel free to add any augmentation
+        for (label_id, uid, fname) in data:
+            try:
+                _, wav = wavfile.read(fname)
+                wav = wav.astype(np.float32) / np.iinfo(np.int16).max
+
+                L = 16000  # be aware, some files are shorter than 1 sec!
+                if len(wav) < L:
+                    continue
+                # let's generate more silence!
+                samples_per_file = 1 if label_id != name2id['silence'] else 20
+                for _ in range(samples_per_file):
+                    if len(wav) > L:
+                        beg = np.random.randint(0, len(wav) - L)
+                    else:
+                        beg = 0
+                    yield dict(
+                        target=np.int32(label_id),
+                        wav=wav[beg: beg + L],
+                    )
+            except Exception as err:
+                print(err, label_id, uid, fname)
+
+    return generator
+
+class SoundCorpusFileStreamer:
+    """Docu goes here"""
+    def __init__(self, data, mode = 'train'):
+        self.data = data
+        self.mode = mode
+    def __iter__(self):
+        if self.mode == 'train':
+            np.random.shuffle(self.data)
+        # Feel free to add any augmentation
+        for (label_id, uid, fname) in self.data:
+            try:
+                _, wav = wavfile.read(fname)
+                wav = wav.astype(np.float32) / np.iinfo(np.int16).max
+
+                L = 16000  # be aware, some files are shorter than 1 sec!
+                if len(wav) < L:
+                    continue
+                # let's generate more silence!
+                samples_per_file = 1 if label_id != name2id['silence'] else 20
+                for _ in range(samples_per_file):
+                    if len(wav) > L:
+                        beg = np.random.randint(0, len(wav) - L)
+                    else:
+                        beg = 0
+                    yield dict(
+                        target=np.int32(label_id),
+                        wav=wav[beg: beg + L],
+                    )
+            except Exception as err:
+                print(err, label_id, uid, fname)
+    def build_corpus(self, max = None):
+        corpus = []
+        k = 0
+        for date in self:
+            if k < 100:
+                corpus.append(date)
+            k += 1
+        return corpus
+
+
+
 trainset, valset = load_data(DATADIR)
+
+gen = SoundCorpusFileStreamer(trainset)
+
+c = gen.build_corpus()
