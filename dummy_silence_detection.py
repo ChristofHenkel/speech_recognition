81d3aca4800a70c15de7a78d185ff83d13877a64
Maureen Tanuadji <mtanuadji@gmail.com>
2017-12-03 10:36:19 +0100

    adjust the thresholds, add zero crossing

--- dummy_silence_detection.py
+++ dummy_silence_detection.py
@@ -8,8 +8,11 @@ from scipy.signal import hilbert, chirp
 from python_speech_features import mfcc, logfbank, delta
 import acoustics
 from scipy.signal import butter, lfilter, freqz, fftconvolve, welch
+from statsmodels.tsa.stattools import acf
+from sklearn.preprocessing import normalize
 
 logging.basicConfig(level=logging.DEBUG)
+
 
 class SilenceDetector:
     def __init__(self):
@@ -17,11 +20,23 @@ class SilenceDetector:
         self.dir_files = 'train/audio/*/*wav'
         self.L = 16000  # length of files
         self.config_padding = True
+        self.white_noise = None
+        self.pink_noise = None
+
+    def config(self):
+        self.white_noise = np.array((
+            (acoustics.generator.noise(16000 * 60, color='white')) / 3) *
+                                    32767).astype(np.int16)
+        self.pink_noise = np.array((
+           (acoustics.generator.noise(16000 * 60, color='pink')) / 3) *
+                                   32767).astype(np.int16)
 
     @staticmethod
     def _do_mfcc(signal):
-        signal = mfcc(signal, samplerate=16000, winlen=0.025, winstep=0.01, numcep=13,
-                      nfilt=26, nfft=512, lowfreq=0, highfreq=None, preemph=0.97,
+        signal = mfcc(signal, samplerate=16000, winlen=0.025, winstep=0.01,
+                      numcep=13,
+                      nfilt=26, nfft=512, lowfreq=0, highfreq=None,
+                      preemph=0.97,
                       ceplifter=22, appendEnergy=True)
         dsignal = delta(signal, N=1)
         ddsignal = delta(dsignal, N=1)
@@ -31,7 +46,7 @@ class SilenceDetector:
     def _read_wav_and_pad(self, fname):
         _, wav = wavfile.read(fname)
         len_wav = wav.shape[0]
-        if len_wav < self.L:    # be aware, some files are shorter than 1 sec!
+        if len_wav < self.L:  # be aware, some files are shorter than 1 sec!
             if self.config_padding:
                 # randomly insert wav into a 16k zero pad
                 padded = np.zeros([self.L])
@@ -52,7 +67,8 @@ class SilenceDetector:
         analytic_signal = hilbert(signal)
         amplitude_envelope = np.abs(analytic_signal)
         instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-        instantaneous_frequency = (np.diff(instantaneous_phase) / (2.0 * np.pi) * fs)
+        instantaneous_frequency = (
+                np.diff(instantaneous_phase) / (2.0 * np.pi) * fs)
         if is_plot:
             samples = int(fs * duration)
             t = np.arange(samples) / fs
@@ -73,7 +89,8 @@ class SilenceDetector:
         nyq = 0.5 * fs
         low_cutoff = low / nyq
         high_cutoff = high / nyq
-        b, a = butter(order, [low_cutoff, high_cutoff], btype='band', analog=False)
+        b, a = butter(order, [low_cutoff, high_cutoff], btype='band',
+                      analog=False)
         return b, a
 
     def butter_bandpass_filter(self, data, low, high, fs, order=5):
@@ -107,7 +124,8 @@ class SilenceDetector:
         n = int(T * fs)  # total number of samples
         t = np.linspace(0, T, n, endpoint=False)
         # "Noisy" data.  We want to recover the 1.2 Hz signal from this.
-        data = np.sin(1.2 * 2 * np.pi * t) + 1.5 * np.cos(9 * 2 * np.pi * t) + 0.5 * np.sin(12.0 * 2 * np.pi * t)
+        data = np.sin(1.2 * 2 * np.pi * t) + 1.5 * np.cos(
+            9 * 2 * np.pi * t) + 0.5 * np.sin(12.0 * 2 * np.pi * t)
 
         # Filter the data, and plot both the original and filtered signals.
         y = self.butter_bandpass_filter(data, cutoff, fs, order)
@@ -121,8 +139,11 @@ class SilenceDetector:
 
         plt.subplots_adjust(hspace=0.35)
 
-    def apply_bandpass_filtering(self, data, low_cutoff=80, high_cutoff=1000, order=4, fs=16000, duration=1.0, is_plot=False):
-        y = self.butter_bandpass_filter(data, low_cutoff, high_cutoff, fs, order)
+    def apply_bandpass_filtering(self, data, low_cutoff=80, high_cutoff=1000,
+                                 order=4, fs=16000, duration=1.0,
+                                 is_plot=False):
+        y = self.butter_bandpass_filter(data, low_cutoff, high_cutoff, fs,
+                                        order)
 
         if is_plot:
             samples = int(fs * duration)
@@ -154,8 +175,17 @@ class SilenceDetector:
             return np.zeros(shape=a.shape)
 
     @staticmethod
-    def apply_threshold(x, threshold):
+    def get_zero_crossing(data):
+        idx_zc = np.where(np.diff(np.sign(data)))[0]
+        return idx_zc
+
+    @staticmethod
+    def apply_threshold_greater(x, threshold):
         return x[np.where(x > threshold)]
+
+    @staticmethod
+    def apply_threshold_smaller(x, threshold):
+        return x[np.where(x < threshold)]
 
     @staticmethod
     def running_mean(x, n):
@@ -164,67 +194,113 @@ class SilenceDetector:
 
     def sd_preprocess(self, fname, is_plot):
         signal = self._read_wav_and_pad(fname)
-        signal = signal - np.mean(signal)
-        signal = self.apply_bandpass_filtering(signal, is_plot=is_plot)
         return signal
 
     def sd_amplitude_envelop(self, signal, threshold, is_plot):
         amplitude_envelop = self.get_amplitude_envelop(signal, is_plot=is_plot)
-        amplitude_envelop = self.apply_threshold(amplitude_envelop, threshold)
-        return amplitude_envelop
+        amplitude_envelop2 = amplitude_envelop - np.mean(amplitude_envelop)
+        idx_thres_ae = np.where(amplitude_envelop > threshold)[0]
+        # plt.plot(amplitude_envelop)
+        # plt.plot(amplitude_envelop2)
+        # normalized_ae = normalize(amplitude_envelop, axis=0).ravel()
+        # filtered_ae = np.zeros(shape=normalized_ae.shape)
+        # filtered_ae[idx_thres_ae] = 1
+        # plt.plot(normalized_ae)
+        # plt.plot(filtered_ae)
+        original_len = len(signal)
+        filtered_len = len(idx_thres_ae)
+        ratio = filtered_len / float(original_len)
+        logging.log(logging.DEBUG, "ratio_ae:"+str(ratio))
+        if ratio > 0.5:
+            return 1
+        else:
+            return 0
 
     def sd_autocorrelation(self, signal, threshold):
-        white_noise = np.array(((acoustics.generator.noise(16000*60, color='white'))/3) * 32767).astype(np.int16)
-        pink_noise = np.array(((acoustics.generator.noise(16000*60, color='pink'))/3) * 32767).astype(np.int16)
-        signal_acorr = self.autocorrelation(signal)
-        # plt.figure()
-        # plt.plot(signal_acorr)
-        # print(signal_acorr)
-        signal_acorr = self.apply_threshold(np.abs(signal_acorr[1:]), threshold)
-        return signal_acorr
+        # signal_acorr = self.autocorrelation(signal)
+        # signal_acorr = self.apply_threshold(np.abs(signal_acorr[1:]),
+        # threshold)
+        acorr = acf(signal, nlags=1, fft=True)
+        logging.log(logging.DEBUG, "acorr:" + str(acorr[1]))
+        if acorr[1] > threshold:
+            return 1
+        else:
+            return 0
 
-    def silence_detection(self, fname, threshold_db=4.0,  threshold_acorr=0.1, is_plot=False):
+    def sd_zero_crossing(self, signal, threshold):
+        idx_zc = self.get_zero_crossing(signal)
+        ratio_zc = len(idx_zc) / float(len(signal))
+        logging.log(logging.DEBUG, "ratio_zc:" + str(ratio_zc))
+        if ratio_zc < threshold:
+            return 1
+        else:
+            return 0
+
+    def silence_detection(self, fname, threshold_db=3.0,
+                          threshold_acorr=0.3, threshold_zero_crossing=0.3,
+                          is_plot=False):
         signal = self.sd_preprocess(fname, is_plot)
-        f, Pxx_spec = welch(signal, 16000, 'flattop', 1024, scaling='spectrum')
+        # f, Pxx_spec = welch(signal, 16000, 'flattop', 1024,
+        # scaling='spectrum')
         # plt.figure()
         # plt.semilogy(f, np.sqrt(Pxx_spec))
         # plt.xlabel('frequency [Hz]')
         # plt.ylabel('Linear spectrum [V RMS]')
         # plt.title(os.path.basename(fname))
+        # n = int(16000 * 0.2)
+        # print(self.running_mean(signal, n))
 
-        n = int(16000 * 0.2)
-        print(self.running_mean(signal, n))
-        signal_amplitude = self.sd_amplitude_envelop(signal, threshold_db, is_plot)
-        signal_acorr = self.sd_autocorrelation(signal, threshold_acorr)
+        w_amplitude = 0.33
+        w_acorr = 0.33
+        w_zero_crossing = 0.33
+        sg_amplitude = self.sd_amplitude_envelop(signal, threshold_db, is_plot)
+        sg_acorr = self.sd_autocorrelation(signal, threshold_acorr)
+        sg_zero_crossing = self.sd_zero_crossing(signal,
+                                                 threshold_zero_crossing)
+        sg = list()
+        sg.append(sg_amplitude)
+        sg.append(sg_acorr)
+        sg.append(sg_zero_crossing)
+        is_speech = np.prod(sg)
+        logging.log(logging.DEBUG, "result:" + ",".join([str(x) for x in sg]))
         if is_plot:
             plt.show()
-        if signal_amplitude.shape[0] > 0 and signal_acorr.shape[0] > 0:
-            return 1
-        else:
-            return 0
+        silence = 1 - is_speech
+        return silence
 
-    def process(self):
+    def testcase(self):
         ps_fname = "/Users/maureen/Documents/Work/kaggle/assets/data_augmentation/silence/pure_silence.wav"
         bn_pathnames = "/Users/maureen/Documents/Work/kaggle/assets/train/audio/_background_noise_/*.wav"
-        dg_pathnames = "/Users/maureen/Documents/Work/kaggle/assets/train/audio/dog/*.wav"
+        dg_pathnames = "/Users/maureen/Documents/Work/kaggle/assets/train" \
+                       "/audio/bed/*.wav"
         test_sil_pathnames = "/Users/maureen/Documents/Work/kaggle_additional_data/label/silence/*.wav"
         bn_fnames = glob.glob(bn_pathnames)
         dg_fnames = glob.glob(dg_pathnames)
         ts_fnames = glob.glob(test_sil_pathnames)
-        threshold_db = 6
-        threshold_acorr = 0.1
-        for fname in bn_fnames:
-            result = self.silence_detection(fname, threshold_db=threshold_db, threshold_acorr=threshold_acorr)
-            print(os.path.basename(fname), result)
+        thres_db = 3
+        thres_acorr = 0.3
+        thres_zero_crossing = 0.3
+        # for fname in ts_fnames:
+        #
+        #     result = self.silence_detection(fname, threshold_db=thres_db,
+        #                                     threshold_acorr=thres_acorr,
+        #                                     threshold_zero_crossing=thres_zero_crossing)
+        #     #print(os.path.basename(fname), result)
+        #     logging.log(logging.DEBUG, "File:" + os.path.basename(fname) +
+        #                 "--" + str(result))
 
-        # for fnames in dg_fnames[0:3]:
-        #     result = self.silence_detection(fnames, threshold_db=threshold_db, threshold_acorr=threshold_acorr)
-        #     print(os.path.basename(fnames), result)
+        for fname in ts_fnames:
+            result = self.silence_detection(fname,
+                                            threshold_db=thres_db,
+                                            threshold_acorr=thres_acorr,
+                                            threshold_zero_crossing=thres_zero_crossing)
+            #print(os.path.basename(fnames), result)
+            logging.log(logging.DEBUG, "File:" + os.path.basename(fname) +
+                        "--" + str(result))
+        plt.show()
 
-
-        #plt.show()
 
 if __name__ == "__main__":
     SC = SilenceDetector()
-    SC.process()
-    #SC.demo_lf()
+    SC.testcase()
+    # SC.demo_lf()
