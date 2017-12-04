2971b4318cbdaa091ce61e1fe437b99f5508b574
Maureen Tanuadji <mtanuadji@gmail.com>
2017-12-04 08:26:13 +0100

    add spectrogram and log filter banks

--- input_features.py
+++ input_features.py
@@ -1,4 +1,6 @@
-from python_speech_features import mfcc, delta
+from python_speech_features import mfcc, delta, logfbank
+import tensorflow as tf
+from scipy import signal
 import numpy as np
 
 # signal is from tensorflow
@@ -9,11 +11,20 @@ import numpy as np
 
 # 2)
 
-#specgram = signal.stft(
-#    wav,
-#    400,  # 16000 [samples per second] * 0.025 [s] -- default stft window frame
-#    160,  # 16000 * 0.010 -- default stride
-#)
+def get_spectrogram(signals, fs=16000, winlen=0.025, winstep=0.1, nfft=512):
+    nooverlap = fs * winstep
+    window_func = ('tukey', winlen)
+    f, t, Sxx = signal.spectrogram(signals, fs=fs, window=window_func,
+        noverlap=nooverlap, nfft=nfft)
+    return f, t, Sxx
+
+
+def get_amplitude_phase(specgram):
+    phase = np.angle(specgram) / np.pi
+    amp = np.log1p(np.abs(specgram))
+    return phase, amp
+
+
 ## specgram is a complex tensor, so split it into abs and phase parts:
 #phase = tf.angle(specgram) / np.pi
 ## log(1 + abs) is a default transformation for energy units
@@ -27,17 +38,28 @@ import numpy as np
 
 # 4)
 
-def stacked_mfcc(signal):
-    signal = mfcc(signal, samplerate=16000, winlen=0.025, winstep=0.01, numcep=13,
-                  nfilt=26, nfft=512, lowfreq=0, highfreq=None, preemph=0.97,
-                  ceplifter=22, appendEnergy=True)
-    dsignal = delta(signal, N=1)
-    ddsignal = delta(dsignal, N=1)
-    signal = np.stack([signal, dsignal, ddsignal], axis=2)
+def stacked_mfcc(signals, fs=16000, winlen=0.25, winstep=0.1,
+                    nfft=512, preemph=0.97):
+    signals = mfcc(signals, samplerate=fs, winlen=winlen, winstep=winstep,
+                  numcep=13, nfilt=26, nfft=nfft, lowfreq=0, highfreq=None,
+                  preemph=preemph, ceplifter=22, appendEnergy=True)
+    dsignals = delta(signals, N=1)
+    ddsignals = delta(dsignals, N=1)
+    signal = np.stack([signals, dsignals, ddsignals], axis=2)
     return signal
 
-#stacked mfcc
 
-# mfcc
-# delta
-# deltadelta
+# 5)
+
+def log_filter_bank(signals, fs=16000, winlen=0.25, winstep=0.1, nfilt=26,
+                    nfft=512, preemph=0.97):
+    log_fb = logfbank(signals, samplerate=fs, winlen=winlen, winstep=winstep,
+                      nfilt=nfilt, nfft=nfft, lowfreq=0, highfreq=None,
+                      preemph=preemph)
+    return log_fb
+
+
+# 6)
+# Maureen: GMM
+
+#if __name__ == "__main__":
