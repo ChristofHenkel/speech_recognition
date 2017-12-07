found bug in batch normalization, changed own test to fname included RNN as top layer

input_features.py
@@ -38,16 +38,23 @@ def get_amplitude_phase(specgram):
 
 # 4)
 
-def stacked_mfcc(signals, fs=16000, winlen=0.25, winstep=0.1,
-                    nfft=512, preemph=0.97):
-    signals = mfcc(signals, samplerate=fs, winlen=winlen, winstep=winstep,
-                  numcep=13, nfilt=26, nfft=nfft, lowfreq=0, highfreq=None,
-                  preemph=preemph, ceplifter=22, appendEnergy=True)
-    dsignals = delta(signals, N=1)
-    ddsignals = delta(dsignals, N=1)
-    signal = np.stack([signals, dsignals, ddsignals], axis=2)
-    return signal
+def stacked_mfcc(signal, num_layers = 2):
+    signal = mfcc(signal, samplerate=16000, winlen=0.025, winstep=0.01, numcep=13,
+                  nfilt=26, nfft=512, lowfreq=0, highfreq=None, preemph=0.97,
+                  ceplifter=22, appendEnergy=True)
 
+    signal_stack = [signal]
+
+    for k in range(num_layers):
+        delta_ = delta(signal_stack[k],N=1)
+        signal_stack.append(delta_)
+
+    if num_layers > 0:
+        signal_stack = np.stack(signal_stack, axis=2)
+    else:
+        signal_stack = signal
+
+    return signal_stack
 
 # 5)
 

predict.py
@@ -1,85 +1,53 @@
 import tensorflow as tf
-import numpy as np
-from glob import glob
 from batch_gen import SoundCorpus
 from architectures import Model5 as Model
 import os
-import pickle
 import logging
 from silence_detection import SilenceDetector
 from input_features import stacked_mfcc
+import pickle
 logging.basicConfig(level=logging.INFO)
 
 class Config:
     soundcorpus_dir = 'assets/corpora/corpus12/'
     batch_size = 298
-    is_training = False
+    is_training = True
     use_batch_norm = True
     keep_prob = 1
-    display_step = 10
-    logs_path = 'models/model0/'
-
 
 cfg = Config()
 
-corpus = SoundCorpus(cfg.soundcorpus_dir, mode='test', fn='test.p.soundcorpus.p')
+test_corpus = SoundCorpus(cfg.soundcorpus_dir, mode='test', fn='test.p.soundcorpus.p')
+silence_corpus = SoundCorpus(cfg.soundcorpus_dir, mode = 'silence', fn='silence.p.soundcorpus.p')
 SC = SilenceDetector()
-batch_gen = corpus.batch_gen(cfg.batch_size)
+corpus_len = test_corpus._get_len()
 
-decoder = corpus.decoder
+
+batch_gen = test_corpus.batch_gen(cfg.batch_size)
+#batch = [b for b in test_corpus]
+decoder = silence_corpus.decoder
 num_classes = len(decoder) -1
 
 model = Model(cfg)
+
 # set_graph Graph
-
-batch_size = cfg.batch_size
-is_training = cfg.is_training
-
-
-training_iters = corpus.len
-
-
-
-
 graph = tf.Graph()
+#tf.reset_default_graph()
 with graph.as_default():
     # tf Graph input
     tf.set_random_seed(3)
     with tf.name_scope("Input"):
         x = tf.placeholder(tf.float32, shape=(None, 99, 13, 3), name="input")
-        # x.set_shape([batch_size, size])
         y = tf.placeholder(tf.int64, shape=(None,), name="input")
         keep_prob = tf.placeholder(tf.float32, name="dropout")
-
-
     with tf.variable_scope('logit'):
         logits = model.calc_logits(x, keep_prob, num_classes)
-        #predictions = tf.nn.softmax(logits)
-
-    #with tf.variable_scope('costs'):
-        #xent = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        #   labels=y, logits=logits)
-        #cost = tf.reduce_mean(xent, name='xent')
-        # cost += self._decay()
-
-        #tf.summary.scalar('cost', cost)
-
-    #with tf.variable_scope('acc'):
         pred = tf.argmax(logits, 1)
-        #correct_prediction = tf.equal(pred, tf.reshape(y, [-1]))
-        #accuracy = tf.reduce_mean(
-        #    tf.cast(correct_prediction, tf.float32), name='accu')
-
-        #tf.summary.scalar('accuracy', accuracy)
     saver = tf.train.Saver()
 
-def submission():
-    fn_model = 'models/model40/model_mfcc_bsize512_e49.ckpt'
-    # %%
-    id2name = corpus.decoder
-    #cfg = Config()
-    # cfg.soundcorpus_fp = 'assets/corpora/corpus7/test.pm.soundcorpus.p'
-    size = 158538
+def prepare_submission(fn_model,fn_out=None):
+    #batch_x = [b['wav'] for b in batch]
+    #batch_y = [b['label'] for b in batch]
 
     submission = dict()
 
@@ -92,6 +60,7 @@ def submission():
         try:
             for (batch_x, batch_y) in batch_gen:
 
+
                 batch_x2 = [stacked_mfcc(b) for b in batch_x]
 
                 if k_batch % 1000 == 0:
@@ -99,20 +68,27 @@ def submission():
                 prediction = sess.run([pred], feed_dict={x: batch_x2, keep_prob: 1.0})
                 for k,p in enumerate(prediction[0]):
                     if SC.is_silence(batch_x[k]):
-                        fname, label = batch_y[k].decode(), 'silence'
+                        #fname, label = batch_y[k].decode(), 'silence'
+                        fname, label = batch_y[k], 'silence'
                     else:
-                        fname, label = batch_y[k].decode(), id2name[p]
+                        #fname, label = batch_y[k].decode(), decoder[p]
+                        fname, label = batch_y[k], decoder[p]
                     submission[fname] = label
                 k_batch += 1
         except EOFError:
             pass
 
-        with open(os.path.join('assets/corpora/corpus12/', 'submission_test7.csv'), 'w') as fout:
-            fout.write('fname,label\n')
-            for fname, label in submission.items():
-                fout.write('{},{}\n'.format(fname, label))
+        if fn_out is not None:
+            with open(os.path.join(cfg.soundcorpus_dir, fn_out), 'w') as fout:
+                fout.write('fname,label\n')
+                for fname, label in submission.items():
+                    fout.write('{},{}\n'.format(fname, label))
+    return submission
 
+def acc():
+    with open(cfg.soundcorpus_dir + 'fname2label.p', 'rb') as f:
+        fname2label = pickle.load(f)
+    comparison = [(p,submission[p],decoder[fname2label[p]]) for p in submission]
+    acc = [a[1] == a[2] for a in comparison].count(True)/len(comparison)
 if __name__ == '__main__':
-
-
-    submission()
+    submission = prepare_submission(fn_model='models/model40/model_mfcc_bsize512_e49.ckpt',fn_out='submission_test7.csv')

predict_on_test_new2.py
@@ -1,13 +1,11 @@
 from create_soundcorpus import SC_Config, SoundCorpusCreator
 import tensorflow as tf
 import numpy as np
-from glob import glob
 from batch_gen import SoundCorpus
 from architectures import Model5 as Model
 import os
 import pickle
 import logging
-from python_speech_features import mfcc, delta
 from input_features import stacked_mfcc
 from silence_detection import SilenceDetector
 
@@ -16,18 +14,21 @@ logging.basicConfig(level=logging.INFO)
 
 class Config:
     soundcorpus_dir = 'assets/corpora/corpus12/'
-    is_training = True
-    use_batch_norm = True
+    is_training = False
+    use_batch_norm = False
     keep_prob = 1
     display_step = 10
 
 cfg = Config()
 silence_corpus = SoundCorpus(cfg.soundcorpus_dir, mode = 'silence')
-test_corpus = SoundCorpus(cfg.soundcorpus_dir,mode='own_test',fn='own_test.p.soundcorpus.p')
+test_corpus = SoundCorpus(cfg.soundcorpus_dir,mode='own_test',fn='own_test_fname.p.soundcorpus.p')
 silence_classifier = SilenceDetector()
 
 batch = [item for item in test_corpus]
 
+
+with open(cfg.soundcorpus_dir + 'fname2label.p','rb') as f:
+    fname2label = pickle.load(f)
 decoder = silence_corpus.decoder
 encoder = silence_corpus.encoder
 num_classes=len(decoder) - 1
@@ -64,21 +65,12 @@ with graph.as_default():
     saver = tf.train.Saver()
 
 
+batch_x = [b['wav'] for b in batch]
+batch_x_mfcc = [stacked_mfcc(b) for b in batch_x]
+batch_y = [b['label'] for b in batch]
 
 
-true_silence_ids = [id for id,item in enumerate(batch) if item['label'] == 11]
-silence_ids = [id for id,item in enumerate(batch) if silence_classifier.is_silence(item['wav'])]
-
-batch2 = [b for id, b in enumerate(batch) if id not in true_silence_ids]
-batch_x = [stacked_mfcc(item['wav']) for item in batch2]
-batch_y = [item['label'] for item in batch2]
-batch_x_incl_silence = [stacked_mfcc(item['wav']) for item in batch]
-batch_y_incl_silence = [item['label'] for item in batch]
-
-batch_y_test = [np.random.randint(0,11) for b in batch_y]
-batch_y_incl_silence_test = [np.random.randint(0,11) for b in batch_y_incl_silence]
-
-def predict(batch_x,batch_y):
+def predict(batch_x_mfcc,batch_y):
     fn_model = 'models/model40/model_mfcc_bsize512_e49.ckpt'
     # %%
 
@@ -88,18 +80,18 @@ def predict(batch_x,batch_y):
         saver.restore(sess, fn_model)
         print("Model restored.")
         k_batch = 0
-        predic = sess.run([pred],feed_dict={x: batch_x_incl_silence, keep_prob: 1.0})
+        submission = {}
+        predic = sess.run([pred],feed_dict={x: batch_all_x_mfcc, keep_prob: 1.0})
         #predic, cm, acc = sess.run([pred,confusion_matrix, accuracy], feed_dict={x: batch_x_incl_silence, y:batch_y_incl_silence_test, keep_prob: 1.0})
-        counter = 0
         predic_incl_silence = predic[0]
         for k, p in enumerate(predic[0]):
-            if silence_classifier.is_silence(batch[k]['wav']):
-                #counter +=1
+            if silence_classifier.is_silence(batch_all[k]['wav']):
+
                 predic_incl_silence[k] = 11
-                print(predic_incl_silence[k] == batch_y_incl_silence[k])
-        print(counter)
-        bool_acc = [predic_incl_silence[k] == batch_y_incl_silence[k] for k,_ in enumerate(batch_y_incl_silence)]
-        acc = bool_acc.count(True)/len(batch_y_incl_silence)
+                #print(predic_incl_silence[k] == batch_y[k])
+            submission[batch_all_y[k].decode()] = predic_incl_silence[k]
+        #bool_acc = [predic_incl_silence[k] == batch_y[k] for k,_ in enumerate(batch_y)]
+        #acc = bool_acc.count(True)/len(batch_y)
         #print(cm)
         #print('Acc',acc)
         k_batch += 1
@@ -181,3 +173,4 @@ acc3 = [prediction[k] == batch_y_incl_silence[k] for k,_ in enumerate(batch_y_in
 
 
 
+[fname2label[s] == submission_own_test[s] for k,s in enumerate(submission_own_test)].count(True)/len(submission_own_test)

train_model.py
@@ -1,30 +1,30 @@
 
 from batch_gen import SoundCorpus, BatchGenerator
 import tensorflow as tf
-from tensorflow.contrib import layers
-import numpy as np
 import time
 import logging
-import pickle
-import os
-from architectures import Model2 as Model
+from architectures import Model5 as Model
 logging.basicConfig(level=logging.DEBUG)
 
 class Config:
     soundcorpus_dir = 'assets/corpora/corpus12/'
     is_training = True
     use_batch_norm = True
-    keep_prob = 1
+    keep_prob = 0.8
     max_gradient = 5
-    tf_seed = 2
+    tf_seed = 4
     learning_rate = 1
-    lr_decay_rate = 0.8
+    lr_decay_rate = 0.9
     lr_change_steps = 100
     display_step = 10
     display_step_val = 50
-    epochs = 20
+    epochs = 50
     epochs_per_save = 1
-    logs_path = 'models/model32/'
+    logs_path = 'models/model42/'
+
+    def save(self):
+        with open(self.logs_path + 'config.txt','w') as f:
+
 
 class BatchParams:
     batch_size = 512
@@ -72,27 +72,21 @@ with graph.as_default():
         y = tf.placeholder(tf.int64, shape=(None,), name="input")
         keep_prob = tf.placeholder(tf.float32, name="dropout")
 
-
-
-    # (128, 12) -> (1)
-    # loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
     with tf.variable_scope('logit'):
         logits = cnn_model.calc_logits(x,keep_prob,num_classes)
         predictions = tf.nn.softmax(logits)
 
     with tf.variable_scope('costs'):
-        xent = tf.nn.sparse_softmax_cross_entropy_with_logits(
-            labels=y, logits=logits)
-        cost = tf.reduce_mean(xent, name='xent')
-        # cost += self._decay()
+        xent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+        cost = tf.reduce_mean(xent, name='xent')#
+
 
         tf.summary.scalar('cost', cost)
 
     with tf.variable_scope('acc'):
         pred = tf.argmax(logits, 1)
         correct_prediction = tf.equal(pred, tf.reshape(y, [-1]))
-        accuracy = tf.reduce_mean(
-            tf.cast(correct_prediction, tf.float32), name='accu')
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accu')
         confusion_matrix = tf.confusion_matrix(tf.reshape(y, [-1]),pred,num_classes)
         tf.summary.scalar('accuracy', accuracy)
     with tf.variable_scope('acc_per_class'):
@@ -106,13 +100,12 @@ with graph.as_default():
     raw_gradients = tf.gradients(cost, tf.trainable_variables())
     gradnorm = tf.global_norm(raw_gradients)
     tf.summary.scalar('grad_norm', gradnorm)
-    gradients, _ = tf.clip_by_global_norm(raw_gradients,
-                                          max_gradient, name="clip_gradients")
+    gradients, _ = tf.clip_by_global_norm(raw_gradients,max_gradient, name="clip_gradients")
     gradnorm_clipped = tf.global_norm(gradients)
     tf.summary.scalar('grad_norm_clipped', gradnorm_clipped)
     iteration = tf.Variable(0, dtype=tf.int64, name="iteration", trainable=False)
     lr_ = tf.Variable(cfg.learning_rate, dtype=tf.float64, name="lr_", trainable=False)
-    decay = tf.Variable(0.95, dtype=tf.float64, name="decay", trainable=False)
+    decay = tf.Variable(cfg.lr_decay_rate, dtype=tf.float64, name="decay", trainable=False)
     steps_ = tf.Variable(100, dtype=tf.int64, name="setps_", trainable=False)
     lr = tf.train.exponential_decay(lr_, iteration,steps_, decay, staircase=True)
     tf.summary.scalar('learning_rate', lr)
@@ -121,14 +114,6 @@ with graph.as_default():
         name="train_step",
         global_step=iteration)
 
-
-
-    #grad_norm_summary = summaryOps.scalarSummary('squared L2-norm of the gradient', Grad_norm)
-    #self.train_writer.add_summary(grad_norm_summary, iteration)
-
-    #pred = tf.argmax(logits, axis=-1)
-    #correct_pred = tf.equal(pred, tf.reshape(y, [-1]))
-    #accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     saver = tf.train.Saver()
     summaries = tf.summary.merge_all()
@@ -142,12 +127,10 @@ def debug_model():
     with tf.Session(graph=graph) as sess:
         init = tf.global_variables_initializer()
         sess.run(init)
-        batch_gen = corpus.batch_gen(cfg.batch_size)
+        batch_gen = advanced_gen.batch_gen()
         batch_x, batch_y = next(batch_gen)
-        cm,l, acc, pred_,y_, id1_ = sess.run([confusion_matrix,logits,accuracy,pred,y,id1], feed_dict={x: batch_x, y: batch_y, keep_prob: cfg.keep_prob})
-        print(cm)
-        print(l, acc)
-        return (cm, l, acc,pred_,y_, id1_)
+        l, kw = sess.run([logits, krw], feed_dict={x: batch_x, y: batch_y, keep_prob: cfg.keep_prob})
+        return l, kw
 
 def aggregate_y(batch_y):
     knowns = [id for id in batch_y if id in [0,1,2,3,4,5,6,7,8,9]]