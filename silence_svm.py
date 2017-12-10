8e82b4cebdc060d543c1250d2d528d593d3d9fe8
Maureen Tanuadji <mtanuadji@gmail.com>
2017-12-10 23:00:56 +0100

    svm: clean up, split the code into functions

--- silence_svm.py
+++ silence_svm.py
@@ -9,7 +9,6 @@ import numpy as np
 import logging
 import glob
 import os
-from sklearn import preprocessing
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
@@ -34,30 +33,34 @@ def extract_features(sd, fnames):
     n = len(fnames)
     for i, fname in enumerate(fnames):
         logging.log(logging.DEBUG, str(i) + "/" + str(n))
-        features = []
         signal = sd.sd_preprocess(fname, is_plot=False)
-        a_corr = sd.autocorrelation(signal)
-        envelope = sd.get_amplitude_envelop(signal)
-        mfcc = sd.read_wav_and_pad(fname)
-        #features.append(a_corr)
-        features_all.append(envelope)
+        # a_corr = sd.autocorrelation(signal)
+        # envelope = sd.get_amplitude_envelop(signal)
+        mfcc = sd._read_wav_and_pad(fname)
+        features_all.append(mfcc)
     return features_all
 
 
-if __name__ == "__main__":
+def get_corpus_subset(fnames, n):
+    indicies = np.random.random_integers(0, len(fnames) - 1, n)
+    fnames = np.asarray(fnames)
+    fnames = fnames[indicies]
+    return fnames
+
+
+def get_balanced_corpus(silence_fnames, speech_fnames, num_of_data=-1,
+                       percentage_of_speech=0.5,
+                       test_size=0.33, random_state=42, is_split=False):
     SD = SilenceDetector()
-    silence_pathnames = 'assets/data_augmentation/silence/background/*.wav'
-    all_train_pathnames = 'assets/train/audio/*/*wav'
-    bn_dir = "assets/train/audio/_background_noise_/"
-    silence_fnames = glob.glob(silence_pathnames)[:2000]
-    speech_fnames = [x for x in glob.glob(all_train_pathnames) if
-                             os.path.dirname(x) is not bn_dir][:2000]
-    test_sil_pathnames = "../../kaggle_additional_data/label/silence/*.wav"
-    test_all_pathnames = "../../kaggle_additional_data/label/*/*.wav"
-    ts_sil_fnames = glob.glob(test_sil_pathnames)
-    sil_dir = "../kaggle_additional_data/label/silence/"
-    ts_no_sil_fnames = [x for x in glob.glob(test_all_pathnames) if
-                        os.path.dirname(x) is not sil_dir]
+    if num_of_data > 0:
+        max_num_of_data = np.min([len(silence_fnames), len(speech_fnames)])
+        if num_of_data > max_num_of_data:
+            num_of_data = max_num_of_data
+        print ("(Max) num of data: " + str(num_of_data))
+        n_speech = int(percentage_of_speech * num_of_data)
+        n_silence = int((1 - percentage_of_speech) * num_of_data)
+        speech_fnames = get_corpus_subset(speech_fnames, n_speech)
+        silence_fnames = get_corpus_subset(silence_fnames, n_silence)
 
     Y_silence = np.empty(len(silence_fnames))
     Y_silence.fill(0)
@@ -66,60 +69,91 @@ if __name__ == "__main__":
 
     Y = np.concatenate([Y_silence, Y_speech], axis=0)
 
-    fnames = silence_fnames + speech_fnames
+    if isinstance(speech_fnames, np.ndarray):
+        fnames = silence_fnames.tolist() + speech_fnames.tolist()
+    else:
+        fnames = silence_fnames + speech_fnames
     data = [(fnames[i], Y[i]) for i in range(len(fnames))]
     np.random.shuffle(data)
     fnames = [x[0] for x in data]
     y = [x[1] for x in data]
     X = extract_features(SD, fnames)
 
-    X_train, X_test, y_train, y_test = \
-        train_test_split(
-            X, y, test_size=0.33, random_state=42)
+    if is_split:
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=test_size,
+                                                            random_state=random_state)
+        return X_train, X_test, y_train, y_test
+    else:
+        return X, y
 
+
+if __name__ == "__main__":
+    silence_pathnames = 'assets/data_augmentation/silence/background/*.wav'
+    all_train_pathnames = 'assets/train/audio/*/*wav'
+    bn_dir = "assets/train/audio/_background_noise_/"
+    silence_fnames = glob.glob(silence_pathnames)
+    speech_fnames = [x for x in glob.glob(all_train_pathnames) if
+                     os.path.dirname(x) is not bn_dir]
+
+    ##################### Load data set
+    X_train, X_test, y_train, y_test = get_balanced_corpus(silence_fnames,
+                                                          speech_fnames, 8000,
+                                                          0.5, is_split=True)
+
+    ##################### Model
     svc_rbf = svm.SVC(kernel='rbf')
-    # scores = cross_val_score(svc_rbf, X_train, y_train, cv=10)
-    # print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
+
+    ###################### Automatic Cross Validation
+    scores = cross_val_score(svc_rbf, X_train, y_train, cv=10)
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+
+###################### Training & Testing
     svc_rbf.fit(X_train, y_train)
 
-    Yt_silence = np.empty(len(ts_sil_fnames))
-    Yt_silence.fill(0)
-    Yt_speech = np.empty(len(ts_no_sil_fnames))
-    Yt_speech.fill(1)
+    ####################### Test 1
+    y_score = svc_rbf.decision_function(X_test)
+    average_precision = average_precision_score(y_test, y_score)
 
-    yt = np.concatenate([Yt_silence, Yt_speech], axis=0)
-    tfnames = ts_sil_fnames + ts_no_sil_fnames
-    Xt = extract_features(SD, tfnames)
+    print('Average precision-recall score: {0:0.2f}'.format(
+        average_precision))
+    precision, recall, _ = precision_recall_curve(y_test, y_score)
+    plt.step(recall, precision, color='b', alpha=0.2,
+             where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2,
+                     color='b')
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title(
+        '2-class Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
+    plt.show()
+
+    ####################### Test 2
+    test_sil_pathnames = "../../kaggle_additional_data/label/silence/*.wav"
+    test_all_pathnames = "../../kaggle_additional_data/label/*/*.wav"
+    ts_sil_fnames = glob.glob(test_sil_pathnames)
+    sil_dir = "../kaggle_additional_data/label/silence/"
+    ts_no_sil_fnames = [x for x in glob.glob(test_all_pathnames) if
+                        os.path.dirname(x) is not sil_dir]
+
+    Xt, yt = get_balanced_corpus(ts_sil_fnames, ts_no_sil_fnames,
+                                 is_split=False)
     output = svc_rbf.predict(Xt)
-    # for i, ot in enumerate(output):
-    #     print(ot, yt[i])
-
     y_score = svc_rbf.decision_function(Xt)
     average_precision = average_precision_score(yt, y_score)
     print('Average precision-recall score: {0:0.2f}'.format(
         average_precision))
-    # y_score = svc_rbf.decision_function(X_test)
-    # print(y_score)
-    # average_precision = average_precision_score(y_test, y_score)
-    #
-    # print('Average precision-recall score: {0:0.2f}'.format(
-    #     average_precision))
-    #
-    # precision, recall, _ = precision_recall_curve(y_test, y_score)
-    #
-    # plt.step(recall, precision, color='b', alpha=0.2,
-    #          where='post')
-    # plt.fill_between(recall, precision, step='post', alpha=0.2,
-    #                  color='b')
-    #
-    # plt.xlabel('Recall')
-    # plt.ylabel('Precision')
-    # plt.ylim([0.0, 1.05])
-    # plt.xlim([0.0, 1.0])
-    # plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
-    #     average_precision))
-    # plt.show()
+
+
+
+
+
+#################### Old code
     # seed = 7
     # np.random.seed(seed)
     #
