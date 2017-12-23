908bfd99e4122de3d2d27b9983323ca51ed9e32b
Christof Henkel <henkel.christof@gmail.com>
2017-12-23 22:24:54 +0100

    added two baselines

--- architectures.py
+++ architectures.py
@@ -290,6 +290,182 @@ class cnn_rnn_v3:
 
         return logits
 
+class cnn_rnn_v4:
+    """like cnn_rnn_v3 but with layer normalization
+    """
+
+    def __init__(self, cfg):
+        pass
+
+    def calc_logits(self, x, keep_prob, num_classes):
+        x2 = x
+        x2 = tf.contrib.layers.layer_norm(x2)
+        x2 = layers.conv2d(x2, num_outputs=54, kernel_size=(4, 70), stride=1, activation_fn=tf.nn.elu)
+        x2 = layers.max_pool2d(x2, kernel_size=(3, 1), stride=1)
+        x2 = tf.contrib.layers.layer_norm(x2)
+        x2 = layers.conv2d(x2, num_outputs=54, kernel_size=(2, 35), stride=1, activation_fn=tf.nn.elu)
+        x2 = layers.max_pool2d(x2, kernel_size=(2, 1), stride=1)
+        x2 = tf.contrib.layers.layer_norm(x2)
+        x2 = layers.conv2d(x2, num_outputs=54, kernel_size=(1, 20), stride=1, activation_fn=tf.nn.elu)
+        x2 = layers.max_pool2d(x2, kernel_size=(2, 1), stride=1)
+
+        x2 = tf.unstack(x2,axis=3)
+        x2 = tf.concat(x2,axis = 2)
+
+
+        # Define a lstm cell
+        with tf.variable_scope('lstm1'):
+            stacked_fw_rnn = []
+            for fw_Lyr in range(2):
+                fw_cell = tf.contrib.rnn.LayerNormBasicLSTMCell(256, forget_bias=1.0,dropout_keep_prob=keep_prob)
+                stacked_fw_rnn.append(fw_cell)
+            fw_multi_cell = tf.contrib.rnn.MultiRNNCell(cells=stacked_fw_rnn, state_is_tuple=True)
+
+        with tf.variable_scope('lstm2'):
+            stacked_bw_rnn = []
+            for bw_Lyr in range(2):
+                bw_cell = tf.contrib.rnn.LayerNormBasicLSTMCell(256, forget_bias=1.0,dropout_keep_prob=keep_prob)
+                stacked_bw_rnn.append(bw_cell)
+            bw_multi_cell = tf.contrib.rnn.MultiRNNCell(cells=stacked_bw_rnn, state_is_tuple=True)
+
+        outputs, _ = tf.nn.bidirectional_dynamic_rnn(fw_multi_cell,bw_multi_cell,x2,dtype=tf.float32)
+        output_fw, output_bw = outputs
+
+        outputs = tf.add(output_fw,output_bw)
+
+        outputs = tf.nn.dropout(outputs, keep_prob=keep_prob)
+        # flatten
+        outputs = tf.contrib.layers.flatten(outputs)
+
+        x3 = layers.fully_connected(outputs, 32, activation_fn=tf.nn.relu)
+
+
+        logits = layers.fully_connected(x3, num_classes, activation_fn=tf.nn.relu)
+
+        return logits
+
+class cnn_rnn_v5:
+    """trying peepholes
+    """
+
+    def __init__(self, cfg):
+        pass
+
+    def calc_logits(self, x, keep_prob, num_classes):
+        x2 = x
+        x2 = layers.conv2d(x2, num_outputs=54, kernel_size=(4, 70), stride=1, activation_fn=tf.nn.elu)
+        x2 = layers.max_pool2d(x2, kernel_size=(3, 1), stride=1)
+        x2 = layers.conv2d(x2, num_outputs=54, kernel_size=(2, 35), stride=1, activation_fn=tf.nn.elu)
+        x2 = layers.max_pool2d(x2, kernel_size=(2, 1), stride=1)
+        x2 = layers.conv2d(x2, num_outputs=54, kernel_size=(1, 20), stride=1, activation_fn=tf.nn.elu)
+        x2 = layers.max_pool2d(x2, kernel_size=(2, 1), stride=1)
+
+        x2 = tf.unstack(x2,axis=3)
+        x2 = tf.concat(x2,axis = 2)
+
+
+        # Define a lstm cell with tensorflow
+        with tf.variable_scope('lstm1'):
+            stacked_fw_rnn = []
+            for fw_Lyr in range(2):
+                fw_cell = tf.contrib.rnn.LSTMCell(256, forget_bias=1.0, state_is_tuple=True, use_peepholes=True)  # or True
+                fw_cell = tf.nn.rnn_cell.DropoutWrapper(fw_cell, output_keep_prob=keep_prob)
+                stacked_fw_rnn.append(fw_cell)
+            fw_multi_cell = tf.contrib.rnn.MultiRNNCell(cells=stacked_fw_rnn, state_is_tuple=True)
+            #fw_reset_state = fw_multi_cell.zero_state(self.batch_params.batch_size, dtype=tf.float32)
+            #fw_state = fw_reset_state
+
+            output, _ = tf.nn.dynamic_rnn(fw_multi_cell, x2, dtype=tf.float32)
+            # Select last output.
+            output = tf.transpose(output, [1, 0, 2])
+            fw_outputs = tf.gather(output, int(output.get_shape()[0]) - 1)
+            #fw_outputs = tf.reshape(fw_output, [-1, 128])
+
+        with tf.variable_scope('lstm2'):
+            stacked_bw_rnn = []
+            for bw_Lyr in range(2):
+                bw_cell = tf.contrib.rnn.LSTMCell(256, forget_bias=1.0, state_is_tuple=True, use_peepholes=True)  # or True
+                bw_cell = tf.nn.rnn_cell.DropoutWrapper(bw_cell, output_keep_prob=keep_prob)
+                stacked_bw_rnn.append(bw_cell)
+            bw_multi_cell = tf.contrib.rnn.MultiRNNCell(cells=stacked_bw_rnn, state_is_tuple=True)
+            #bw_reset_state = bw_multi_cell.zero_state(self.batch_params.batch_size, dtype=tf.float32)
+            #bw_state = bw_reset_state
+
+            bw_output, _ = tf.nn.dynamic_rnn(bw_multi_cell, x2, dtype=tf.float32)
+            # Select last output.
+            bw_output = tf.transpose(bw_output, [1, 0, 2])
+            bw_outputs = tf.gather(bw_output, int(bw_output.get_shape()[0]) - 1)
+
+
+        #outputs = fw_outputs[-1] + bw_outputs[-1]
+        outputs = fw_outputs + bw_outputs
+        outputs = tf.nn.dropout(outputs, keep_prob=keep_prob)
+        #x2 = layers.conv2d(x2, 32, 1, 1, activation_fn=tf.nn.elu)   # we can use conv2d 1x1 instead of dense
+        x3 = layers.fully_connected(outputs, 32, activation_fn=tf.nn.relu)
+
+
+        logits = layers.fully_connected(x3, num_classes, activation_fn=tf.nn.relu)
+        return logits
+
+class cnn_rnn_v3_small:
+    """Builds a standard convolutional model.
+    This is roughly the network labeled as 'cnn-trad-fpool3' in the
+    'Convolutional Neural Networks for Small-footprint Keyword Spotting' paper:
+    http://www.isca-speech.org/archive/interspeech_2015/papers/i15_1478.pdf
+    """
+
+    def __init__(self, cfg):
+        pass
+
+    def calc_logits(self, x, keep_prob, num_classes):
+        x2 = x
+        #x2 = layers.conv2d(x2, num_outputs=16, kernel_size=(4, 70), stride=1, activation_fn=tf.nn.elu)
+        #x2 = layers.max_pool2d(x2, kernel_size=(3, 1), stride=1)
+        #x2 = layers.conv2d(x2, num_outputs=16, kernel_size=(2, 35), stride=1, activation_fn=tf.nn.elu)
+        #x2 = layers.max_pool2d(x2, kernel_size=(2, 1), stride=1)
+        #x2 = layers.conv2d(x2, num_outputs=16, kernel_size=(1, 20), stride=1, activation_fn=tf.nn.elu)
+        #x2 = layers.max_pool2d(x2, kernel_size=(2, 1), stride=1)
+        x2 = layers.conv2d(x2, num_outputs=16, kernel_size=(1, 10), stride=1, activation_fn=tf.nn.elu)
+        x2 = layers.max_pool2d(x2, kernel_size=(2, 1), stride=1)
+        x2 = layers.conv2d(x2, num_outputs=16, kernel_size=(1, 5), stride=1, activation_fn=tf.nn.elu)
+        x2 = layers.max_pool2d(x2, kernel_size=(2, 1), stride=1)
+
+        x2 = tf.unstack(x2,axis=3)
+        x2 = tf.concat(x2,axis = 2)
+
+
+        # Define a lstm cell
+        with tf.variable_scope('lstm1'):
+            stacked_fw_rnn = []
+            for fw_Lyr in range(1):
+                fw_cell = tf.contrib.rnn.BasicLSTMCell(64, forget_bias=1.0, state_is_tuple=True)  # or True
+                fw_cell = tf.nn.rnn_cell.DropoutWrapper(fw_cell, output_keep_prob=keep_prob)
+                stacked_fw_rnn.append(fw_cell)
+            fw_multi_cell = tf.contrib.rnn.MultiRNNCell(cells=stacked_fw_rnn, state_is_tuple=True)
+
+        with tf.variable_scope('lstm2'):
+            stacked_bw_rnn = []
+            for bw_Lyr in range(1):
+                bw_cell = tf.contrib.rnn.BasicLSTMCell(64, forget_bias=1.0, state_is_tuple=True)  # or True
+                bw_cell = tf.nn.rnn_cell.DropoutWrapper(bw_cell, output_keep_prob=keep_prob)
+                stacked_bw_rnn.append(bw_cell)
+            bw_multi_cell = tf.contrib.rnn.MultiRNNCell(cells=stacked_bw_rnn, state_is_tuple=True)
+
+        outputs, _ = tf.nn.bidirectional_dynamic_rnn(fw_multi_cell,bw_multi_cell,x2,dtype=tf.float32)
+        output_fw, output_bw = outputs
+
+        outputs = tf.add(output_fw,output_bw)
+
+        outputs = tf.nn.dropout(outputs, keep_prob=keep_prob)
+        # flatten
+        outputs = tf.contrib.layers.flatten(outputs)
+
+        x3 = layers.fully_connected(outputs, 16, activation_fn=tf.nn.relu)
+
+
+        logits = layers.fully_connected(x3, num_classes, activation_fn=tf.nn.relu)
+
+        return logits
 
 class cnn_trad_fpool3:
     """Builds a standard convolutional model.
